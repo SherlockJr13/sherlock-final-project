c4e1f379c80dd71cf8d028c30ce2edc7eaff5bd2
Riansyah Fazar <riansyahfazarramadhan@gmail.com>
2024-06-10 11:46:22 +0700

    adding code for kmeans and kmedoids plot

--- clusterviz.R
+++ clusterviz.R
@@ -3,6 +3,7 @@ library(sf)
 library(dplyr)
 library(readxl)
 library(RColorBrewer)
+library(viridis)
 
 # Read the shapefile
 indo_sf <- st_read("E:/GitHub/sherlock-final-project/map_cluster/indo_province_map.shp")
@@ -10,16 +11,23 @@ print(head(indo_sf))
 print(indo_sf)
 
 # Read the cluster data from the Excel file
-cluster_data <- read_excel("E:/GitHub/sherlock-final-project/best_kmedoids_cluster.xlsx", sheet='Sheet1')
+cluster_data <- read_excel("E:/GitHub/sherlock-final-project/cluster_summary.xlsx", sheet='Sheet1')
+cluster_data$kmedoids <- cluster_data$kmedoids +1
 
 # Merge the shapefile data with the cluster data
 merged_data <- indo_sf %>%
   left_join(cluster_data, by = c("PROVINSI" = "province"))
 
-library(viridis)
 # Visualize with ggplot2 using viridis color palette
 ggplot(data = merged_data) +
-  geom_sf(aes(fill = factor(cluster_index))) +
-  scale_fill_viridis_d(option = "viridis", name = "Cluster") +
+  geom_sf(aes(fill = factor(kmeans))) +
+  scale_fill_viridis_d(option = "viridis", name = "kmeans") +
   theme_minimal() +
-  labs(title = "Cluster Visualization of 34 Provinces in Indonesia")
+  labs(title = "Klaster Tingkat Kemiskinan Indonesia - K-means")
+
+# Visualize with ggplot2 using viridis color palette
+ggplot(data = merged_data) +
+  geom_sf(aes(fill = factor(kmedoids))) +
+  scale_fill_viridis_d(option = "viridis", name = "kmedoids") +
+  theme_minimal() +
+  labs(title = "Klaster Tingkat Kemiskinan Indonesia - K-medoids")
